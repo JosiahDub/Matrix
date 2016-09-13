be8b6ae464bd8b4803685833968865f87019b47d
Josiah White <josiahwhite@gmail.com>
2016-09-12 22:59:11 -0400

    Added more constructors and add functions

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -3,25 +3,49 @@
 
 #include "Matrix.h"
 
-Matrix::Matrix(uint rows, uint cols)
+Matrix::Matrix(uint rows, uint cols):
+	num_rows(rows),
+	num_cols(cols)
 {
-	num_rows = rows;
-	num_cols = cols;
 	matrix = new double*[rows];
-	for (uint i = 0; i < cols; ++i)
+	for (uint i = 0; i < rows; ++i)
 	{
 		matrix[i] = new double[cols];
 	}
 }
 
-Matrix::Matrix(const i_list & list)
+Matrix::Matrix(const i_list & list) :
+	num_rows(list.size()),
+	num_cols(list.begin()->size())
 {
-
+	matrix = new double*[num_rows];
+	int matrix_row_num = 0;
+	for (auto list_row : list)
+	{
+		matrix[matrix_row_num] = new double[num_cols];
+		int matrix_col_num = 0;
+		for (auto list_num : list_row)
+		{
+			matrix[matrix_row_num][matrix_col_num] = list_num;
+			++matrix_col_num;
+		}
+		++matrix_row_num;
+	}
 }
 
-Matrix::Matrix(const Matrix & m)
+Matrix::Matrix(const Matrix & m) :
+	num_rows(m.num_rows),
+	num_cols(m.num_cols)
 {
-
+	matrix = new double*[num_rows];
+	for (uint i = 0; i < num_rows; ++i)
+	{
+		matrix[i] = new double[num_cols];
+		for (uint j = 0; j < num_cols; ++j)
+		{
+			matrix[i][j] = m.matrix[i][j];
+		}
+	}
 }
 
 Matrix::~Matrix()
@@ -33,17 +57,31 @@ Matrix::~Matrix()
 	delete [] matrix;
 }
 
-/*Matrix Matrix::add(double s) const
+Matrix Matrix::add(double s) const
 {
-	
+	for (uint i = 0; i < num_rows; ++i)
+	{
+		for (uint j = 0; j < num_cols; ++j)
+		{
+			matrix[i][j] += s;
+		}
+	}
+	return *this;
 }
 
 Matrix Matrix::add(const Matrix & m) const
 {
-
+	for (uint i = 0; i < this->num_rows; ++i)
+	{
+		for (uint j = 0; j < this->num_cols; ++j)
+		{
+			this->matrix[i][j] += m.at(i, j);
+		}
+	}
+	return *this;
 }
 
-Matrix Matrix::subtract(double s) const
+/*Matrix Matrix::subtract(double s) const
 {
 
 }
@@ -88,7 +126,7 @@ double & Matrix::at(uint row, uint col)
 	return matrix[row][col];
 }
 
-/*const double & Matrix::at(uint row, uint col) const
+const double & Matrix::at(uint row, uint col) const
 {
-
-}*/
+	return matrix[row][col];
+}
--- Matrix/MatrixTest.cpp
+++ Matrix/MatrixTest.cpp
@@ -7,6 +7,19 @@ int main()
 	uint columns = 2;
 	Matrix m(rows, columns);
 	m.at(0, 0) = 14.0;
-	std::cout << m.at(0, 0) << std::endl;
+	std::cout << "normal m(0, 0): " << m.at(0, 0) << std::endl;
+
+	//19
+	m.add(5);
+	std::cout << "added m(0, 0): " << m.at(0, 0) << std::endl;
+
+	Matrix m_list({ {1, 2},
+					{3, 4}});
+	// 1
+	std::cout << "list m(0, 0): " << m_list.at(0, 0) << std::endl;
+
+	m.add(m_list);
+	//20
+	std::cout << "added list m(0, 0): " << m_list.at(0, 0) << std::endl;
 	return 0;
 }
